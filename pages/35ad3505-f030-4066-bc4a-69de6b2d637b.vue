f2a023c2bb27c58d10bcb375d957a1d7860ea607
Jerry Bruwes <jbruwes@icloud.com>
2025-08-30 14:12:19 +0200

    2025-08-30

---
+++ pages/35ad3505-f030-4066-bc4a-69de6b2d637b.vue
@@ -0,0 +1,10 @@
+<template>
+  <TresCanvas window-size>
+    <TresPerspectiveCamera></TresPerspectiveCamera>
+    <router-view></router-view>
+  </TresCanvas>
+</template>
+
+<script lang="js" setup>
+import { TresCanvas } from '@tresjs/core';
+</script>
---
+++ pages/addd1af0-3f52-4a46-a164-cf51221bcf2c.vue
@@ -0,0 +1,6 @@
+<template>
+</template>
+
+<script lang="js" setup>
+
+</script>
